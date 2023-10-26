6b44606f1044f12d3ee6937c862d71e58bfbcb06
mwhal012 <145363243+mwhal012@users.noreply.github.com>
2023-10-26 14:16:20 -0400

    Add files via upload

--- earlyviz-abdata.R
+++ earlyviz-abdata.R
@@ -12,6 +12,9 @@ abdata = "ab_data.csv" |>
 	)
 
 abdata |>
+	count(group, landing_page)
+
+abdata |>
 	count(group, landing_page, converted)
 summary(abdata)
 
@@ -26,33 +29,31 @@ abdata |>
 totals = abdata |>
 	count(group, landing_page) |>
 	mutate(total = n, .keep = "unused")
+# calculate the proportions of each population that converted
 proportions = abdata |>
 	count(group, landing_page, converted) |>
 	inner_join(totals) |>
-	mutate(prop = n/total, .keep = "unused")
+	mutate(prop = n/total)
 
 ggplot(
 	data = proportions |>
 		filter(converted == 1)
-) + geom_point(
+) + geom_count(
 	mapping = aes(
 		x = landing_page,
 		y = group,
 		colour = prop,
-		size = prop
+		size = total
 	)
+) + scale_size_area(
+	max_size = 50,
+	guide = NULL
+) + theme_classic() + labs(
+	title = "Store membership signup rates",
+	subtitle = "by landing page and treatment/control"
+) + theme(
+	plot.title = element_text(size = rel(2)),
+	axis.line = element_blank(),
+	axis.ticks = element_blank(),
+	axis.title = element_blank()
 )
-
-regready = abdata |>
-	mutate(
-		landing_page = landing_page |>
-			case_match("old_page" ~ 0, "new_page" ~ 1) |>
-			as.numeric(),
-		group = group |>
-			case_match("control" ~ 0, "treatment" ~ 1) |>
-			as.numeric(),
-		timestamp = as.double(timestamp) - min(as.double(timestamp))
-	)
-reg = lm(converted ~ group*landing_page*timestamp, data = regready)
-summary(reg)
-
